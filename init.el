8ed82f983c177dbef153e671f6321dfaf9cc2e90
Mathias Nielsen (MNIE) <MNIE@oticon.com>
2020-04-24 13:48:15 +0200

    removed org-roam

--- init.el
+++ init.el
@@ -144,7 +144,7 @@
         +dragndrop       ; drag & drop files/images into org buffers
         ;;+hugo            ; use Emacs for hugo blogging
         ;;+jupyter        ; ipython/jupyter support for babel
-        +roam
+        ;;+roam
         +pandoc          ; export-with-pandoc support
         +pomodoro        ; be fruitful with the tomato technique
         +present)        ; using org-mode for presentations
